1265ca56caca5396a619419a90fff2c4c5ce34a4
Serge-Harnyk <xenonchikmaxxx@gmail.com>
2016-02-18 17:33:25 +0200

    Add Local FS

--- src/main/java/AmazonEntryPoint.java
+++ src/main/java/AmazonEntryPoint.java
@@ -4,8 +4,7 @@ import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.S3Object;
-import com.amazonaws.services.s3.model.S3ObjectInputStream;
-import org.apache.hadoop.fs.*;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.s3a.S3AInputStream;
 
 import java.io.*;
@@ -27,6 +26,9 @@ public class AmazonEntryPoint {
                 System.out.println("Use Amazon SDK");
                 S3Object s3object = s3Client.getObject(new GetObjectRequest(bucketName, key));
                 stream = s3object.getObjectContent();
+            } else if(args[0].equals("local")) {
+                System.out.println("Use Local FS");
+                stream = new FileInputStream(new File("ltl3.csv"));
             } else {
                 System.out.println("Use Hadoop FS");
                 stream = new S3AInputStream(bucketName, key, 0l, s3Client, new FileSystem.Statistics("s3a"));
@@ -42,20 +44,12 @@ public class AmazonEntryPoint {
 
     private static void process(InputStream input)
             throws IOException {
-        // Read one text line at a time and display.
         BufferedReader reader = new BufferedReader(new
                 InputStreamReader(input));
-//        File f = new File("/tmp/dest.csv");
-//        FileWriter fr = new FileWriter(f);
-//        BufferedWriter br  = new BufferedWriter(fr);
         while (true) {
             String line = reader.readLine();
             if (line == null) break;
-//            br.write(line);
-//            br.newLine();
 
         }
-//        br.close();
-
     }
 }
